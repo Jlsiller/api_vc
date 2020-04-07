ec5c1672b6205f0bdcad4e9c7c578295122a415b
Siller <jose.luis.siller@gmail.com>
2020-04-06 23:51:38 -0500

    morgan en index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const express= require('express');
 const app = express();
 
+const morgan = require('morgan');
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+
 app.listen(3000,() => {
     console.log('server on port ${3000} ');
 
