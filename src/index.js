0a2fe48e6fa84a7b598de93235848dbce558788a
Siller <jose.luis.siller@gmail.com>
2020-04-07 00:22:14 -0500

    commit modemon

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const express= require('express');
 const app = express();
-
 const morgan = require('morgan');
+
+//settings
+app.set('port',process.env.PORT || 3000);
+
+//middlewares para soportar los datos que vamos a recibir
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
+//routes
+app.get('/',(req,res) =>{
+    res.send('hello world');
+});
 
-app.listen(3000,() => {
-    console.log('server on port ${3000} ');
+//starting the server
+app.listen(app.get('port'),() => {
+    console.log(`server on port ${app.get('port')}`);
 
 });
