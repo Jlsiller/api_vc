a64e75710057cb71fb40c9045cc9dbec68e46fb9
Siller <jose.luis.siller@gmail.com>
2020-04-09 18:39:15 -0500

    commit final trabajando

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 //app.use(require('./routes/index'));
 //app.use(require('./routes/movies'));
 app.use('/api/movies',require('./routes/movies'));
+app.use('/api/users',require('./routes/users'));
 
 //starting the server
 app.listen(app.get('port'),() => {
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const router = Router();
+const _ = require('underscore');
 
 const movies = require('../sample.json');
 console.log(movies);
@@ -15,17 +16,48 @@ router.post('/',(req,res) =>{
        const newMovie ={id,...req.body};
        movies.push(newMovie);
       console.log(newMovie);
-       res.json(movies);
+       res.json(movies); 
    }
    else {
        //res.send('falta un dato error en envio');
         res.status(500).json({error: 'hubo un error'});
         
    };
-  
-  
-
 });
+
+  router.delete('/:id',(req,res) => {
+      const {id} =req.params;
+
+   console.log(req.params);
+   _.each(movies,(movie,i) => {
+       if (movie.id == id) {
+           movies.splice(i,1);
+       }
+   })
+   res.send('movies');
+   
+  });
+
+router.put('/:id',(req,res) =>{
+    const {id}= req.params;
+    const {title,director,year,rating}=req.body;
+    if (title &&director&&year&&rating){
+        _.each(movies,(movie,i) => {
+            if (movie.id== id){
+                movie.title=title,
+                movie.director=director;
+                movie.year=year;
+                movie.rating=rating;
+            }
+        });
+        res.json(movies);
+    }
+    else {
+        res.status(500).json({error: 'hubo un error en el put'});
+    }
+    }
+);
+
 
 /*
 router.get('/',(req,res) =>{
